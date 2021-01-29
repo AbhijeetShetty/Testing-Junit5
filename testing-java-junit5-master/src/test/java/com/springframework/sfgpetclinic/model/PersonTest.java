c7df3e211069bbe111e4a5ca58954a6ffb621d0f
Abhijeet Shetty <abhijeetshetty001@gmail.com>
2021-01-29 12:38:52 +0100

    Grouped Assertion

---
+++ testing-java-junit5-master/src/test/java/com/springframework/sfgpetclinic/model/PersonTest.java
@@ -0,0 +1,21 @@
+package com.springframework.sfgpetclinic.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PersonTest {
+
+	@Test
+	void groupedAssertions() {
+
+		// given
+		Person person = new Person(1L, "Joe", "Buck");
+
+		// then
+		assertAll("Test Props Set", 
+				() -> assertEquals("Joe", person.getFirstName(), "First Name failed"),
+				() -> assertEquals("Buck", person.getLastName(), "Second Name failed"));
+	}
+
+}

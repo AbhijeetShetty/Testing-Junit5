1ce71e89aa96f179dafe399b118d6552a6ff04ba
Abhijeet Shetty <abhijeetshetty001@gmail.com>
2021-01-29 14:17:20 +0100

    @DisplayName and  @Disable annotations set

--- testing-java-junit5-master/src/test/java/com/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ testing-java-junit5-master/src/test/java/com/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -3,6 +3,7 @@ package com.springframework.sfgpetclinic.controllers;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class IndexControllerTest {
@@ -14,6 +15,7 @@ class IndexControllerTest {
 		controller = new IndexController();
 	}
 
+	@DisplayName("Test proper view name is returned for index page")
 	@Test
 	void testIndex() {
 		assertEquals("index", controller.index());
@@ -21,6 +23,7 @@ class IndexControllerTest {
 		assertEquals("index", controller.index(), () -> "Another expensive message, displayed only in case of failure");
 	}
  
+	@DisplayName("Test exception")
 	@Test
 	void testOupsHandler() {
 		assertTrue("notimplemented".equals(controller.oupsHandler()),

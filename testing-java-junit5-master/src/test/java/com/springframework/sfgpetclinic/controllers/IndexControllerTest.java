a54da951a1342ce4a1d0ce15251fd1c88ec481f1
Abhijeet Shetty <abhijeetshetty001@gmail.com>
2021-01-29 14:56:27 +0100

    Conditional Junit test

--- testing-java-junit5-master/src/test/java/com/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ testing-java-junit5-master/src/test/java/com/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -2,9 +2,17 @@ package com.springframework.sfgpetclinic.controllers;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.Duration;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+import org.junit.jupiter.api.condition.EnabledOnJre;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.JRE;
+import org.junit.jupiter.api.condition.OS;
 
 import com.springframework.sfgpetclinic.exceptions.ValueNotFoundException;
 
@@ -28,8 +36,53 @@ class IndexControllerTest {
 	@DisplayName("Test exception")
 	@Test
 	void testOupsHandler() {
-		assertThrows(ValueNotFoundException.class, 
-				() -> controller.oopsHandler());
+		assertThrows(ValueNotFoundException.class, () -> controller.oopsHandler());
 	}
 
+	@Disabled("Demo of Timeout")
+	@Test
+	void testTimeOut(){		
+		assertTimeout(Duration.ofMillis(100),()->{
+			Thread.sleep(5000);
+			System.out.println("I am here");
+		} );		
+	}
+	
+	@Disabled("Demo of TimeOutPrempt")
+	@Test
+	void testTimeOutPrempt(){		
+		assertTimeoutPreemptively(Duration.ofMillis(100),()->{
+			Thread.sleep(5000);
+			System.out.println("I am in preemptive test");
+		} );		
+	}
+	
+	@EnabledOnOs(OS.WINDOWS)
+	@Test
+	void testMeOnWin() {
+		
+	}
+	
+	@EnabledOnOs(OS.MAC)
+	@Test
+	void testMeOnMac() {
+		
+	}
+	@EnabledOnJre(JRE.JAVA_8)
+	@Test
+	void testMeOnJava8() {
+		
+	}
+	
+	@EnabledOnJre(JRE.JAVA_11)
+	@Test
+	void testMeOnJava11() {
+		
+	}
+	
+	@EnabledIfEnvironmentVariable(named="USERNAME",matches ="AS")
+	@Test
+	void testMeIfUserAS(){
+		
+	}
 }

77c0f57af6c7d1bb744cc03dfc4fc769757fb1f6
Abhijeet Shetty <abhijeetshetty001@gmail.com>
2021-01-30 21:30:09 +0100

    parameter test added and reporting

--- testing-java-junit5-master/src/test/java/com/springframework/sfgpetclinic/model/OwnerTest.java
+++ testing-java-junit5-master/src/test/java/com/springframework/sfgpetclinic/model/OwnerTest.java
@@ -2,13 +2,23 @@ package com.springframework.sfgpetclinic.model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import com.springframework.sfgpetclinic.CustomArgsProvider;
 import com.springframework.sfgpetclinic.ModelTest;
 
-
-class OwnerTest implements ModelTest{
+class OwnerTest implements ModelTest {
 
 	@Test
 	void dependentAssertion() {
@@ -22,9 +32,46 @@ class OwnerTest implements ModelTest{
 						() -> assertEquals("Buck", owner.getLastName(), "last name did not match")),
 				() -> assertAll("Owner Properties",
 						() -> assertEquals("New York", owner.getCity(), "City did not match"),
-						() -> assertEquals("1234567890", owner.getTelephone(), "Phone Number did not match"))
-				);
+						() -> assertEquals("1234567890", owner.getTelephone(), "Phone Number did not match")));
 
 	}
 
+	@DisplayName("Value source test")
+	@ParameterizedTest(name = "[{index}]-{arguments}")
+	@ValueSource(strings = { "This", "is", "a", "list", "of", "string", "for", "testing" })
+	void testValueSource(String val) {
+		System.out.println(val);
+	}
+
+	@DisplayName("Enum Source Test")
+	@ParameterizedTest(name = "[{index}]-{arguments}")
+	@EnumSource(OwnerType.class)
+	void enumTest(OwnerType ownerType) {
+		System.out.println(ownerType);
+	}
+
+	@DisplayName("CSV Input Test")
+	@ParameterizedTest(name = "[{index}] {arguments}")
+	@CsvSource({ "FL, 1, 1", "OH, 2, 2", "MI, 3, 1" })
+	void csvInputTest(String stateName, int val1, int val2) {
+		System.out.println(stateName + " = " + val1 + ":" + val2);
+	}
+
+	@DisplayName("Method Provider Test")
+	@ParameterizedTest(name = "[{index}] {arguments}")
+	@MethodSource("getargs")
+	void fromMethodTest(String stateName, int val1, int val2) {
+		System.out.println(stateName + " = " + val1 + ":" + val2);
+	}
+
+	static Stream<Arguments> getargs() {
+		return Stream.of(Arguments.of("FL", 5, 1), Arguments.of("OH", 2, 8), Arguments.of("MI", 3, 5));
+	}
+
+	@DisplayName("Custom Provider Test")
+	@ParameterizedTest(name = "[{index}] {arguments}")
+	@ArgumentsSource(CustomArgsProvider.class)
+	void fromCustomProviderTest(String stateName, int val1, int val2) {
+		System.out.println(stateName + " = " + val1 + ":" + val2);
+	}
 }
--- testing-java-junit5-master/src/test/java/com/springframework/sfgpetclinic/model/PersonTest.java
+++ testing-java-junit5-master/src/test/java/com/springframework/sfgpetclinic/model/PersonTest.java
@@ -4,14 +4,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 
 import com.springframework.sfgpetclinic.ModelTest;
 
+class PersonTest implements ModelTest {
 
-class PersonTest implements ModelTest{
-  
 	@Test
 	void groupedAssertions() {
 
@@ -19,16 +20,20 @@ class PersonTest implements ModelTest{
 		Person person = new Person(1L, "Joe", "Buck");
 
 		// then
-		assertAll("Test Props Set", 
-				() -> assertEquals("Joe", person.getFirstName(), "First Name failed"),
+		assertAll("Test Props Set", () -> assertEquals("Joe", person.getFirstName(), "First Name failed"),
 				() -> assertEquals("Buck", person.getLastName(), "Second Name failed"));
 	}
-	
+
 	@RepeatedTest(value = 10, name = "{displayName} : {currentRepetition} - {totalRepetitions} ")
 	@DisplayName("My test repeated")
-	@Test
 	void testRepeated() {
-		
+
 	}
- 
+
+	@RepeatedTest(5)
+	void testRepeatedWithID(TestInfo testInfo, RepetitionInfo reptTestInfo) {
+		System.out.println(testInfo.getDisplayName() + "-" + reptTestInfo.getCurrentRepetition() + " - "
+				+ reptTestInfo.getTotalRepetitions());
+	}
+
 }
